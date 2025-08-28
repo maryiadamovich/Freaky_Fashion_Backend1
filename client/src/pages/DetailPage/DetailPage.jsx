4275ae824445c42994f94155ce4042f792d3aeae
Maryia <marika.tr77@gmail.com>
2025-08-28 11:25:32 +0200

    DetailPage lifts up

--- client/src/pages/DetailPage/DetailPage.jsx
+++ client/src/pages/DetailPage/DetailPage.jsx
@@ -2,11 +2,16 @@ import { useWindowSizeValues } from '../../contexts/useWindowSizeValues';
 import Section_main from "./Section_main";
 import Section_variants from "./Section_variants";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function DetailPage() {
 
   const { isMobil } = useWindowSizeValues();
   const { name } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [name]);
   
   return (
     <main className="px-[1rem]">
--- client/src/pages/DetailPage/Section_main.jsx
+++ client/src/pages/DetailPage/Section_main.jsx
@@ -1,24 +1,29 @@
 import { useWindowSizeValues } from '../../contexts/useWindowSizeValues';
 import Button from "../../components/Button/Button";
-import { shuffleArray } from "../../contexts/shuffleArray";
-import useChangeTitle from '../../contexts/useChangeTitle';
+import { useContext } from "react";
+import { DataContext } from "../../contexts/dataServer";
+import { useChangeTitle } from '../../contexts/useChangeTitle';
 import { useLocation } from 'react-router-dom';
 
 export default function Section_main({ name }) {
     const { isFull, isMobil } = useWindowSizeValues();
     const location = useLocation();
+    const { products } = useContext(DataContext);
 
     const isDetailProductRoute = location.pathname.startsWith('/product');
-
     useChangeTitle(name, isDetailProductRoute);
 
-    const productDetail = shuffleArray(1, name);
-
-    if (!productDetail || productDetail.length === 0) {
+    //find a product from the Context
+    const productDetail = products.find(product => 
+        product.name.toLowerCase() === name.toLowerCase()
+    );
+    
+    if (!productDetail) {
+        console.log('Product not found - returning early');
         return <p>Product not found</p>;
     }
 
-    const photo = `https://placehold.co/400x600/grey/white?text=${productDetail[0].name}`;
+    const photo = `https://placehold.co/400x600/grey/white?text=${productDetail.name}`;
 
     return (
         <section className={`my-4 ${!isMobil ? "grid grid-cols-2" : ""}`}>
@@ -26,12 +31,12 @@ export default function Section_main({ name }) {
                 <img className="w-full h-auto border" src={photo} alt="Main photo" />
             </div>
             <div className={`text-left  ${isFull ? "max-w-2/3" : ""} ${!isMobil ? "ml-8" : ""}`}>
-                <h1 className="text-2xl font-bold">{productDetail[0].name}</h1>
-                <span>{productDetail[0].label}</span>
+                <h1 className="text-2xl font-bold">{productDetail.name}</h1>
+                <span>{productDetail.label}</span>
                 <p className='pb-4'>
-                    {productDetail[0].description}
+                    {productDetail.description}
                 </p>
-                <span className='block'>{productDetail[0].price} SEK</span>
+                <span className='block'>{productDetail.price} SEK</span>
                 <Button
                     size={`${isMobil ? "w-full" : "w-1/2"}`}
                     text="Lägg i varukorg"
