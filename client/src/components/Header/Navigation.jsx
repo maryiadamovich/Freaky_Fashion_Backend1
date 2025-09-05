b08a6e9f642dddefb4d27bb344050d96d4a8b445
Maryia <marika.tr77@gmail.com>
2025-09-05 15:45:54 +0200

    Add dynamic navigation

--- client/src/components/Header/Navigation.jsx
+++ client/src/components/Header/Navigation.jsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { NavLink, useLocation } from "react-router-dom";
+import { useContext } from 'react';
+import { DataContext } from '../../contexts/dataServer';
 
 export default function Navigation({ css: cssProp, ...rest }) {
 
@@ -19,18 +21,21 @@ export default function Navigation({ css: cssProp, ...rest }) {
     const location = useLocation();
     const isLocation = location.pathname.startsWith('/product');
 
+    const { products } = useContext(DataContext);
+    const categories = [...new Set(products.map(item => item.kategori).filter(Boolean))];
+
     return (
         <nav css={[containerStyle, cssProp]} {...rest}>
             <ul>
-                <li><NavLink to="/" className={({ isActive }) => { return isActive ? "text-lime-500" : "text-black"; }}>
-                    Kläder
-                </NavLink></li>
-                <li><NavLink to="/search" className={({ isActive }) => { return isActive ? "text-lime-500" : "text-black"; }}>
-                    Accessoarer
-                </NavLink></li>
-                <li><NavLink to="/admin/products" className="text-black">
-                    Skor
-                </NavLink></li>
+                {categories.map((category, index) => (
+                    <li key={index}>
+                        <NavLink to={`/`} className={({ isActive }) =>
+                            isActive ? "text-black font-bold" : "text-black"
+                        }>
+                            {category}
+                        </NavLink>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
